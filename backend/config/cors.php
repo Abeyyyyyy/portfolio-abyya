e1a3b36fdd621d5d3353e597d02e93637f902377
Abeyyyyyy <hamdanabiyya@gmail.com>
2026-04-17 09:22:05 +0700

    fix: allow all origins for cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,16 +1,34 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'https://aby-arteezy.vercel.app', 
-        'https://aby-arteezyvercel-2tuwzpcp1-abeyyyyyys-projects.vercel.app', // Domain preview vercel kamu
-        'http://localhost:5173'
-    ],
+
+    'allowed_origins' => ['*'], // Ini akan mengizinkan semua domain Vercel kamu
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => false, // Ubah ke false jika menggunakan '*'
+
+    'supports_credentials' => false, // Wajib false karena kita pakai '*' di atas
+
 ];
